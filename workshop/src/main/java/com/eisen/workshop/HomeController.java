ac92b1ee3e86d96bd936e3c832a1dd7dbaf12ffd
eisen <eisen1990@naver.com>
2017-02-06 15:38:53 +0900

    root-context.xml

--- workshop/src/main/java/com/eisen/workshop/HomeController.java
+++ workshop/src/main/java/com/eisen/workshop/HomeController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +17,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eisen.Member.Member;
+import com.eisen.Member.MemberDAOService;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	private MemberDAOService memberDAOService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(HttpServletRequest request) {
 		ModelAndView result = new ModelAndView();
 		ArrayList<Member> member = new ArrayList<Member>();
 		
+		member = memberDAOService.getMembers();
 		result.setViewName("home");
 		result.addObject("result", member);
 		
